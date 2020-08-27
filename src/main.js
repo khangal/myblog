af952e77b138ba1ec6fa43417784e57964c65c08
Khangal Jargalsaikhan <khangal.j@irbis.sg>
2020-08-27 18:37:55 +0800

    fix dateformat

--- src/main.js
+++ src/main.js
@@ -12,8 +12,7 @@ export default function (Vue, { router, head, isClient }) {
   Vue.filter('dateFormat', function(value) {
     if (!value) return
 
-    const d = new Date(+value)
-
+    let d = new Date(value)
     const year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(d)
     const month = new Intl.DateTimeFormat('en', { month: 'numeric' }).format(d).padStart(2, '0')
     const day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(d).padStart(2, '0')
