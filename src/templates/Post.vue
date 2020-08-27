d3e1f48c7ee894d03c899e8a69ca7c22e6e00251
Khangal Jargalsaikhan <khangal.j@irbis.sg>
2020-08-27 10:22:15 +0800

    include sass configuration

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,8 +1,17 @@
 <template>
   <Layout>
-    single post
+    <div class="foo">
+      single post
+    </div>
   </Layout>
 </template>
+
+<style lang="scss" scoped>
+.foo {
+  background-color: $red;
+}
+
+</style>
 
 <page-query>
 query ($id: ID!) {
